7a0de50384ac05f8d9bebf3ef20964a4ca1e5c60
Krzysztof Kwiecień <kkwiecien00@gmail.com>
2016-07-06 17:17:24 +0200

    do dropdown and disappearing text, prep for slider

---
+++ js/app.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  //drop-down menu
+  var dropDownButton = document.querySelector('.dropDownButton');
+
+  for (var i=0; i<dropDownButton.children.length; i++) {
+    dropDownButton.children[i].style.visibility = 'hidden';
+  }
+
+  dropDownButton.addEventListener('mouseover', function(event){
+    for (var i=0; i<dropDownButton.children.length; i++) {
+      this.children[i].style.visibility = 'visible';
+      this.style.height = '115px';
+      this.children[i].style.bottom = '0';
+    }
+  });
+  dropDownButton.addEventListener('mouseout', function(event){
+    for (var i=0; i<dropDownButton.children.length; i++) {
+      this.children[i].style.visibility = 'hidden';
+      this.style.height = '15px';
+      this.children[i].style.bottom = '-100px';
+    }
+  });
+
+  //disappearing text boxes
+  var textBox1 = document.querySelector('.previewOne');
+  var textBox2 = document.querySelector('.previewTwo');
+
+  textBox1.addEventListener('mouseover', function(event){
+    this.children[1].style.visibility = 'hidden';
+  });
+  textBox1.addEventListener('mouseout', function(event){
+    this.children[1].style.visibility = 'visible';
+  });
+  textBox2.addEventListener('mouseover', function(event){
+    this.children[1].style.visibility = 'hidden';
+  });
+  textBox2.addEventListener('mouseout', function(event){
+    this.children[1].style.visibility = 'visible';
+  });
+
+  //image slider
+
+});
